69bfc092e401d3729e96efa85bb0ecea1b41cbb6
marios4371 <cs04371@uoi.gr>
2022-10-09 16:23:11 +0300

    Import some of entities

---
+++ src/main/java/com/example/ourclotheeshop/OurClotheEshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.ourclotheeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurClotheEshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurClotheEshopApplication.class, args);
+    }
+
+}
